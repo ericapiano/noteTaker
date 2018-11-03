e9b56059f55355a1ebe0903ebe97a6ea7529782b
Erica Piano <ericapiano@Ericas-MacBook-Pro.local>
2018-11-03 13:40:23 -0400

    adding notes is working

--- Solved/public/assets/js/notes.js
+++ Solved/public/assets/js/notes.js
@@ -1,27 +1,30 @@
+$('document').ready(() => {
+  var runNoteQuery = function() {
+    $.ajax({
+      url: "http://localhost:3000/api/notes",
+      method: "GET"
+    }).then(function(noteData) {
+      $(".notesContent").empty();
+      noteData.forEach(element => {
+        $(".notesContent").append('<p>'+JSON.stringify(element)+'</p>')
+      });
 
-var runNoteQuery = function() {
-  $.ajax({
-    url: "/api/notes",
-    method: "GET"
-  }).then(function(noteData) {
-    $("notesContent").append(noteData)
+    console.log(noteData);
+    });
+    
+  };
 
-  console.log(noteData);
+  $("#noteButton").on("click", function(event) {
+    event.preventDefault();
+    var newNote = {
+      title: $("#title").val().trim(),
+      note: $("#note").val().trim()
+    }
+    $.post("http://localhost:3000/api/notes", newNote).done(function(data){
+      runNoteQuery();
+    })
   });
-  
-};
 
-$("#noteButton").on("click", function(event) {
-  event.preventDefault();
-
-  var newNote = {
-    title: $("#title").val().trim(),
-    note: $("#note").val().trim()
-  }
-  $.post("/api/notes", newNote).done(function(data){
-    $("notesContent").append(data)
-  })
+    
+  runNoteQuery();
 });
-
-  
-runNoteQuery();
