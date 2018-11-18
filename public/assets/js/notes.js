0f005c4e990cad48ed68007f96f7cd4449f14e68
Erica Piano <ericapiano@Ericas-MacBook-Pro.local>
2018-11-17 20:37:45 -0500

    style changes

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -6,20 +6,27 @@ $('document').ready(() => {
     }).then(function(noteData) {
       $(".card-body").empty();
       noteData.forEach(element => {
-        var h = $('<h2>')
+        var card = $('<div>').addClass('card')
+        var h = $('<h2 class="my-h2">');
         var p = $('<p>');
-        var b = $('<button>')
-
+        var b = $('<button>');
+        var i = $('<i class="fas fa-trash-alt fa-2x">');
+        var cardBody = $('<div>').addClass('card-body noteText');
 {/* <i class="fas fa-trash-alt float-right text-danger delete-note"></i> */}
 
 
 
         h.text(element.title)
         p.text(element.note)
+        // <i class="fas fa-trash-alt"></i>
         // b.attr("class", "fas fa-trash-alt float-right text-danger delete-note")
         b.attr("data-value", element.id)
         b.attr("class", "delete-button")
-        $('.card-body').append(h,p,b);
+        b.append(i);
+        cardBody.append(h,p,b);
+        card.append(cardBody);
+
+        $('#notesWrapper').append(card);
 
         
       });
