944b0273439729dee215c7db19153bc32d26f94d
Erica Piano <ericapiano@Ericas-MacBook-Pro.local>
2018-11-20 18:12:55 -0500

    gird box working

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,8 @@
 INSERT INTO notes (title, note)
 VALUES ("Grocery List", "milk, detergent, peanut butter, eggs, paper towels, oranges");
 INSERT INTO notes (title, note)
-VALUES ("Saturday", "cat adoption day at petsmart!");
+VALUES ("Saturday", "cat adoption day at petsmart in Bridgewater!");
 INSERT INTO notes (title, note)
-VALUES ("School", "DON'T FORGET! homework 13 due 11/10/18!!");
+VALUES ("School", "DON'T FORGET! homework 13 due Saturday !!");
 INSERT INTO notes (title, note)
 VALUES ("Work", "working Sunday 1:30-11");
